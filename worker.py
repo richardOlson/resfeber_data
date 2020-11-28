4c89c0123b2c82abf57def029207a488e0a6050a
Richard Olson <8571182+richardOlson@users.noreply.github.com>
2020-11-27 22:29:21 -0700

    build function for average price

--- worker.py
+++ worker.py
@@ -26,9 +26,22 @@ def create_staging(cursor, ):
         """
     )
 
-
+def get_avg(tuple_list):
+    """
+    This function will get the average of the tuplelist that is passed in
+    to it. 
+    """
+    the_val = 0
+    # getting the avg of the tuple by doing a loop
+    for tup in tuple_list:
+        the_val += tup[0] # getting the price out of the tuple 
+    ans = the_val/len(tuple_list)
+    return ans
 
 def send_csv_psql(connection, csvfile, tableName):
+    """
+    This is a function that will fill in the data into the postresql database
+    """
     sql = "COPY %s FROM STDIN WITH CSV HEADER DELIMITER AS ','"
     thefile = open(csvfile, 'r')
     table = tableName
@@ -80,12 +93,39 @@ def return_avg_price(lat, lon, room_type, num_nights):
     """
     This function is to find the price average for those items that it pulls 
     from the query."""
+    # getting the connection
+    try:
+        conn = psycopg2.connect(dbname=database, password=password, host=host, user=username)
+    except:
+        print("There was a problem with the connection to the database")
+    finally:
+        # getting the geolacation and the bounding box
+        # Instaciating the GeoLocation class
+        geoClass = GeoLocation.from_degrees(deg_lat=lat, deg_lon= lon)
+        # getting the bound coord
+        bounding = geoClass.bounding_locations(geoClass.dist_kilo, geoClass.EARTH_RADIUS)
+        # calling the query function
+        sql_obj = query(bounding_coord=bounding, loc=geoClass, connection=conn, num_nights=num_nights, room_type=room_type)
+        # fetching the results
+        cur = conn.cursor()
+        cur.execute(sql_obj)
+        # getting the info
+        tuple_list = cur.fetchall()
+        # closing the connections
+        cur.close()
+        conn.close()
+        if len(tuple_list) >= 1: # this will mean that there is a value that is in the tuplelist
+            # calling to get the average price 
+            price = get_avg(tuple_list=tuple_list)
+        else:
+            price = "There was nothing in the list of values"
+        return price
 
 
 if __name__ == "__main__":
 
     # getting the connection
-    conn = psycopg2.connect(dbname=database, password=password, host=host, user=username)
+    #conn = psycopg2.connect(dbname=database, password=password, host=host, user=username)
     
     # # doing the staging the create table
     # with conn.cursor() as cursor:
@@ -95,24 +135,23 @@ if __name__ == "__main__":
     # # C:\Users\rich\Richard_python\Lambda\labs\resfeber_data\radian_df.csv
     
     # Instaciating the GeoLocation class
-    geoClass = GeoLocation.from_degrees(deg_lat=43.717869, deg_lon= -110.317117)
+    #geoClass = GeoLocation.from_degrees(deg_lat=43.717869, deg_lon= -110.317117)
 
-    print(f"The radius is : {geoClass.radius}")
+    #print(f"The radius is : {geoClass.radius}")
 
     # getting the bound coord
-    bounding = geoClass.bounding_locations(geoClass.dist_kilo, geoClass.EARTH_RADIUS)
+    #bounding = geoClass.bounding_locations(geoClass.dist_kilo, geoClass.EARTH_RADIUS)
     # passing into the function
-    sql_ob = query(bounding_coord=bounding, loc=geoClass, connection=conn, num_nights=5, room_type='Private room')
-    print(sql_ob.as_string(conn))
+    #sql_ob = query(bounding_coord=bounding, loc=geoClass, connection=conn, num_nights=5, room_type='Private room')
     #print(sql_ob.as_string(conn))
-    curs = conn.cursor()
-    curs.execute(sql_ob)
+    #print(sql_ob.as_string(conn))
+    #curs = conn.cursor()
+    #curs.execute(sql_ob)
 
-    res = curs.fetchall()
+    #res = curs.fetchall()
     
-    print(res)
-    print(f"the length of the results is {len(res)}")
+    #print(res)
+    #print(f"the length of the results is {len(res)}")
+    price = return_avg_price(lat=40.713051, lon=-74.007233, room_type="Private room", num_nights=1)
 
-    curs.close()
-    conn.close()
-    
+    print(f"The price is the following: {price}")
